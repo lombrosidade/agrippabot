ff0401fbbd1caba2dd245b76e3afce969380db3a
lombrosidade <mdgjohnny@gmail.com>
2023-08-11 18:54:06 -0300

    Added two path helper files and docstrings

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,25 @@
 from typing import List
+import os
 import re
 import random
 
 
-# General exception for file handling errors
 class FileReadError(Exception):
+    """
+    General exception class for handling file errors.
+    """
     pass
 
-# Sanitizes each line and makes sure their word count is within limits
-
-
 def sanitize_line(line: str) -> str:
+    """
+    Sanitizes a line of text and ensures its word count is within limits.
+
+    Args:
+        line (str): The input line to sanitize.
+
+    Returns:
+        str: The sanitized line if it meets criteria, otherwise an empty string.
+    """
     no_brackets = re.sub(r'\[.*?(\s|$|.*])', '', line.strip())
     no_space_before_punctuation = r'\s+([.,;!?:])\s*'
     line_sanitized = re.sub(no_space_before_punctuation, r'\1 ', no_brackets)
@@ -23,6 +32,18 @@ def sanitize_line(line: str) -> str:
 
 
 def read_lines(file_path: str) -> List[str]:
+    """
+    Reads lines from a text file, sanitizes them, and returns a list of valid lines.
+
+    Args:
+        file_path (str): The path to the text file to read.
+
+    Returns:
+        List[str]: A list of sanitized lines from the text file.
+        
+    Raises:
+        FileReadError: If the file is not found or an error occurs while reading.
+    """
     try:
         with open(file_path, 'r') as file:
             lines = []
@@ -32,11 +53,48 @@ def read_lines(file_path: str) -> List[str]:
                     if sanitize_line(sentence) != "":
                         lines.append(sentence)
             return lines
-    except FileNotFoundError:
-        raise FileReadError(f"File not found: {file_path}")
-    except Exception as e:
-        raise FileReadError(f"An error occurred: {e}")
+    except FileNotFoundError as exc:
+        raise FileReadError("File not found: %s", exc)
+    except Exception as general_exception:
+        raise FileReadError("An error occurred: %s", generalexception)
 
 
 def get_random_line(lines: List) -> str:
+    """
+    Returns a random line from a list of lines.
+
+    Args:
+        lines (List): A list of lines to choose from.
+
+    Returns:
+        str: A randomly selected line from the input list.
+    """
     return random.choice(lines)
+
+def get_script_directory() -> str:
+    """
+    Returns the absolute path of the script's directory.
+
+    Returns:
+        str: The absolute path of the script's directory.
+    """
+    directory = os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__))
+    )
+    return directory
+
+def get_text_file(dir, text_path):
+    """
+    Returns the absolute path of the text file within a directory.
+
+    Args:
+        dir (str): The directory containing the text file.
+        text_path (str): The relative path to the text file.
+
+    Returns:
+        str: The absolute path of the text file.
+    """
+    text_path = os.path.join(dir,
+                             text_path)
+    abs_text_path = os.path.abspath(text_path)
+    return abs_text_path
